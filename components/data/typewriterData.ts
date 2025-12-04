19818a6ae63961d18c2e9061c26b11d629eb3d29
Derek Cheng <dca130@sfu.ca>
2025-12-04 03:22:35 -0800

    Update typewriterData.ts

--- components/data/typewriterData.ts
+++ components/data/typewriterData.ts
@@ -1,4 +1,4 @@
 export const words = [
-  { text: "Derek", className: "text-blue-500 dark:text-blue-400" },
+  { text: " Derek", className: "text-blue-500 dark:text-blue-400" },
   { text: "Cheng" },
 ];
